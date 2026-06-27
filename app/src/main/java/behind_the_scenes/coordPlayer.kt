004f1f1ea0cc05615216af7fd38e922122389db7
Vozhd Works <glevoid5@gmail.com>
2026-06-27 23:58:55 +0530

    e
    Signed-off-by: Vozhd Works <glevoid5@gmail.com>

--- app/src/main/java/behind_the_scenes/coordPlayer.kt
+++
@@ -1,9 +0,0 @@
-package behind_the_scenes
-
-import android.app.Service
-import android.os.Bundle
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.weaponizerzstudio.fieryescalation_gpsrts.MainActivity
-
-//slop is too bad for my health will add foreground service here for location
-class coordPlayer : Service()
